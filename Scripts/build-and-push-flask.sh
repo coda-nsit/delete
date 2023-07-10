aca52cc6ea96abd484d79aec094a80d0d75626ee
ribanerjee <ribanerjee@microsoft.com>
2023-07-10 00:20:44 -0700

    fixed error which is why version was not getting picked

--- Scripts/build-and-push-flask.sh
+++ Scripts/build-and-push-flask.sh
@@ -1,4 +1,4 @@
 echo "Building Flask image with version is: $1";
-docker build -t ribanerjee.azurecr.io/flask:$1 ../Flask
+docker build -t "ribanerjee.azurecr.io/flask:$1" ../Flask
 docker push ribanerjee.azurecr.io/flask
 docker image ls
